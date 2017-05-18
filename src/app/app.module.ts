efb0979443609e4d5ccdc4aa9fdf045e1b2742f2
pradeeepsharma <pradeep.sharma@bdindia.in>
2017-05-18 15:50:09 +0530

    adding material theme project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';
+import {
+  MdToolbarModule, MdButtonModule,
+  MdIconModule
+} from '@angular/material';
+
+const MD_COMPONENTS = [
+  MdToolbarModule, MdButtonModule,
+  MdIconModule
+];
+
 
 import { AppComponent } from './app.component';
 
@@ -12,8 +25,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
-  ],
+    HttpModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+  ].concat(MD_COMPONENTS),
   providers: [],
   bootstrap: [AppComponent]
 })
